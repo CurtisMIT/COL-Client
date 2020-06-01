f1f4b169748c98b225666ae3ef4d9e5f65b77d1e
Karming Chin <karmingc@hotmail.com>
2020-05-31 23:52:26 -0400

    autosize update

--- src/components/SubmitPage/Content/Job.vue
+++ src/components/SubmitPage/Content/Job.vue
@@ -73,7 +73,7 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import Continue from '../BottomButton/Continue.vue'
-// import autosize from 'autosize'
+import autosize from 'autosize'
 
 @Component({
     components: {
